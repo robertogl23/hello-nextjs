3138af32923c52b0554dc61d2490763d3e6bd29c
robertogl23 <dev.robertgl@gmail.com>
2020-04-19 14:29:57 -0500

    primer commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Layout from '../components/MyLayout.js';
+
+const aboutPageContent = <p>This is the about page</p>;
+
+export default function About() {
+return <Layout>{aboutPageContent}</Layout>
+
+  
+}
